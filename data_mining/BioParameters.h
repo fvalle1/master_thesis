d316548288dd9dea15367893fd7556020f1ecba7
Filippo Valle <filippovalle@aim.com>
2019-04-29 12:14:41 +0200

    updates

--- data_mining/BioParameters.h
+++ data_mining/BioParameters.h
@@ -5,18 +5,36 @@
 #ifndef THESIS_DATA_MINING_BIOPARAMETERS_H
 #define THESIS_DATA_MINING_BIOPARAMETERS_H
 
+enum species{
+    kHomoSapiens,
+    kMouse
+};
 
 class BioParameters {
 public:
     static constexpr int getENSLenght(){
-        //return 15;//Home sapiens
-        return 18; //mouse
+        switch(fSpecie){
+            case kHomoSapiens:
+                return 15; //Home sapiens
+            case kMouse:
+                return 18; //mouse
+        }
     }
 
     static constexpr int getSampleIdLenght(){
-        //return 36;//TCGA
-        return 18; //mouse
+        switch(fSpecie){
+            case kHomoSapiens:
+                return 36;//TCGA
+            case kMouse:
+                return 18; //mouse
+        }
     }
+
+private:
+    BioParameters() = default;
+    ~BioParameters() = default;
+
+    static const species fSpecie=kMouse;
 };
 
 
