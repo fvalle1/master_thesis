7550c4de1954b431c9466ce3a60851bf3bb30d75
Filippo Valle <filippo.valle@edu.unito.it>
2019-04-30 16:11:00 +0200

    add means in python metaprogramming

--- data_mining/GraphGenerator_Python.cxx
+++ data_mining/GraphGenerator_Python.cxx
@@ -25,13 +25,14 @@ void sampling(int statisticsRepeat=1){
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(sampling_overloads, sampling, 0, 1)
 
-void statistics(bool saveOccurrences=true){
+void statistics(bool saveOccurrences=true, bool considerZeros=true){
     auto model = new MainTable();
     model->read("mainTable.csv", saveOccurrences);
+    model->SaveMeansVariances("mainTable.csv", considerZeros);
     model->~MainTable();
 }
 
-BOOST_PYTHON_FUNCTION_OVERLOADS(statistics_overloads, statistics, 0, 1)
+BOOST_PYTHON_FUNCTION_OVERLOADS(statistics_overloads, statistics, 0, 2)
 
 
 BOOST_PYTHON_MODULE(graphgenerator_ext)
@@ -39,7 +40,8 @@ BOOST_PYTHON_MODULE(graphgenerator_ext)
     using namespace boost::python;
 
     def("statistics", statistics, statistics_overloads(
-            boost::python::arg("saveAbundancesOccurrences")=true)
+            boost::python::arg("saveAbundancesOccurrences")=true,
+            boost::python::arg("considerZeros")=true)
     );
     def("sampling", sampling, sampling_overloads(
             boost::python::arg("averageOver")=1)
