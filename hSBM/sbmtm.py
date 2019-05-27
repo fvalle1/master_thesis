cb3a2bf366190736e9109f3a0278409a72ce45ee
Filippo Valle <filippovalle@aim.com>
2019-05-27 16:44:09 +0200

    add savedata

--- hSBM/sbmtm.py
+++ hSBM/sbmtm.py
@@ -565,7 +565,7 @@ class sbmtm():
         else:
             return n_td_tw
 
-    def plot_topic_dist(l):
+    def plot_topic_dist(self, l):
         groups = self.model.groups[l]
         p_w_tw = groups['p_w_tw']
         fig=plt.figure(figsize=(12,10))
@@ -584,14 +584,14 @@ class sbmtm():
         plt.colorbar()
         fig.savefig("p_tw_d_%d.png"%l)
 
-    def savedata():
+    def savedata(self):
         for i in range(len(self.state.get_levels())-2)[::-1]:
             print("Saving level %d"%i)
             self.model.print_topics(l=i)
             self.model.print_topics(l=i, format='tsv')
             self.plot_topic_dist(i)
             plot_topic_dist(i)
-            self.state.get_levels()[i]
+            e = self.state.get_levels()[i].get_matrix()
             plt.matshow(e.todense())
             plt.savefig("mat_%d.png"%i)
         self.model.print_summary()
