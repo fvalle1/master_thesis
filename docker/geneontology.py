76c5997fa6702141db86bb3c5998835f808030f5
Filippo Valle <filippovalle@aim.com>
2019-08-23 15:45:00 +0200

    push pipeline files

---
+++ docker/geneontology.py
@@ -0,0 +1,15 @@
+import gseapy as gs
+from tableanalyser import get_symbol
+
+def get_ontology_df(topic, background):
+    gene_ontology = gs.enrichr(topic, gene_sets='GO_Molecular_Function_2018,GO_Biological_Process_2018,GO_Cellular_Component_2018,Human_Phenotype_Ontology,GTEx_Tissue_Sample_Gene_Expression_Profiles_up,GTEx_Tissue_Sample_Gene_Expression_Profiles_down,Tissue_Protein_Expression_from_Human_Proteome_Map,KEGG_2019_Human,NCI-60_Cancer_Cell_Lines', cutoff=0.05).results
+    return gene_ontology[gene_ontology['Adjusted P-value']<5e-1].loc[:,['Term','Adjusted P-value', 'Gene_set']]
+
+def ensg_to_symbol(series):
+    symbols = []
+    for g in series:
+        try:
+            symbols.append(get_symbol(g))
+        except:
+            pass
+    return symbols
