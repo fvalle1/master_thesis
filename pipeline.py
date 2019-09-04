9ba5fb3a87fb4a974a233b2b64ce54dba55092a7
Filippo Valle <filippovalle@aim.com>
2019-09-04 13:28:41 +0200

    use real parameters in pipeline

--- pipeline.py
+++ pipeline.py
@@ -3,5 +3,5 @@ import papermill as pm
 pm.execute_notebook(
    'Pipeline.ipynb',
    'Pipeline_out.ipynb',
-   parameters = dict(alpha=0.6, ratio=0.1)
+   parameters = dict(workingdir=r"/Volumes/GoogleDrive/My Drive/tesi_magistrale/tesi/gtex/hsbm/miRNA")
 )
