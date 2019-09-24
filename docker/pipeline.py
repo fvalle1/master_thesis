f198c2d8f4173f1cd15e4cb4fdc7fbd3e56df789
Filippo Valle <filippo.valle@edu.unito.it>
2019-09-24 21:21:20 +0200

    add docker libs

--- docker/pipeline.py
+++ docker/pipeline.py
@@ -3,5 +3,7 @@ import papermill as pm
 pm.execute_notebook(
    'Pipeline.ipynb',
    'Pipeline_out.ipynb',
-   parameters = dict(alpha=0.6, ratio=0.1)
+   parameters = dict(
+   workingdir="/home/filippo/tacos/files/",
+   execdir="/home/filippo/tacos/")
 )
